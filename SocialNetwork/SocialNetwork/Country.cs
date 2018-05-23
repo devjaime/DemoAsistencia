270521797558324986d5c973d10d3461303e750e
jaime <jhernandez.hs@icloud.com>
2018-05-23 03:04:33 -0400

    Initial check-in of module SocialNetwork

---
+++ SocialNetwork/SocialNetwork/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SocialNetwork
+{
+    public class Country
+    {
+        
+            public string CountryCode { get; set; }
+            public string CountryName { get; set; }
+            public string ContinentName { get; set; }
+            public string Capital { get; set; }
+            public string AreaInSqKm { get; set; }
+            public string Population { get; set; }
+            public string CurrencyCode { get; set; }
+            public string Languages { get; set; }
+
+    }
+}
---
+++ SocialNetwork/SocialNetwork/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+using System.Net;
+using System.IO;
+
+
+using System.Xml;
+
+
+namespace SocialNetwork
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        
+        }
+
+
+    }
+}
